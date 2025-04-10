3c585b8163c208319d35d6bbd9c4b8bce6cc6533
parkhasol <hasol1229@handong.ac.kr>
2025-04-11 01:06:57 +0900

    modify hw2

--- pard_5th_parkhasol_instagram/src/pages/component/sidebar.tsx
+++ pard_5th_parkhasol_instagram/src/pages/component/sidebar.tsx
@@ -1,28 +1,41 @@
-import Link from 'next/link'
-import styles from '../styles/sidebar.module.css'
+import Link from 'next/link';
+import styles from '../styles/sidebar.module.css';
 
-interface SidebarProps {
-  className?: string;
-}
+const menuItems = [
+  { label: '홈', icon: 'Home', path: '#' },
+  { label: '검색', icon: 'Search', path: '#' },
+  { label: '릴스', icon: 'Reels', path: '#' },
+  { label: '메시지', icon: 'Share', path: '#' },
+  { label: '알림', icon: 'Like', path: '#' },
+  { label: '만들기', icon: 'More', path: '#' },
+  { label: '프로필', icon: 'Profile', path: '/mypage' },
+];
 
-export default function Sidebar({ className }: SidebarProps) {
+export default function Sidebar() {
   return (
-    <aside className={`${styles.sidebar} ${className}`}>
-      <img src="/img/Company logo.png" className={styles.logo} alt="Instagram Logo"/>
+    <aside className={styles.sidebar}>
+      <img src="/img/Vector.png" className={styles.logo} alt="Instagram Logo" />
+      
       <ul className={styles.menu}>
-        <li><img src="/img/Home.png" className={styles.icon} alt="홈" /> 홈</li>
-        <li><img src="/img/Search.png" className={styles.icon} alt="검색" /> 검색</li>
-        <li><img src="/img/Reels.png" className={styles.icon} alt="릴스" /> 릴스</li>
-        <li><img src="/img/Share.png" className={styles.icon} alt="메시지" /> 메시지</li>
-        <li><img src="/img/Like.png" className={styles.icon} alt="알림" /> 알림</li>
-        <li><img src="/img/More.png" className={styles.icon} alt="만들기" /> 만들기</li>
-        <li>
-          <Link href="/mypage" className={styles.link}>
-            <img src="/img/Profile.png" className={styles.icon} alt="프로필" /> 프로필
-          </Link>
-        </li>
+        {menuItems.map((item) => (
+          <li key={item.label}>
+            <Link href={item.path} className={styles.link}>
+              <img
+                src={`/img/${item.icon}.png`}
+                className={styles.icon}
+                alt={item.label}
+              />
+              {item.label}
+            </Link>
+          </li>
+        ))}
       </ul>
 
+      {/* 더 보기 버튼 (사이드바 하단) */}
+      <div className={styles.more}>
+        <img src="/img/More.png" className={styles.icon} alt="더 보기" />
+        더 보기
+      </div>
     </aside>
-  )
+  );
 }
--- pard_5th_parkhasol_instagram/src/pages/mypage.tsx
+++ pard_5th_parkhasol_instagram/src/pages/mypage.tsx
@@ -1,39 +1,50 @@
-import Sidebar from './component/sidebar';
-import styles from './styles/mypage.module.css';
-import styles1 from './styles/sidebar.module.css';
+import { useState } from "react";
+import Sidebar from "./component/sidebar";
+import styles from "./styles/mypage.module.css";
 
-export default function MyPage() {
+export default function Profile() {
+  const [posts] = useState([
+    { id: 1, image: "/img/1.JPG" },
+    { id: 2, image: "/img/2.JPG" },
+    { id: 3, image: "/img/3.jpeg" },
+    { id: 4, image: "" },
+    { id: 5, image: "" },
+    { id: 6, image: "" },
+  ]);
+  
+
   return (
-    <div className={styles.pageWrapper}>
-      <Sidebar className={styles1.sidebar} />
+    <div className={styles.container}>
+      <Sidebar />
+
       <main className={styles.main}>
-        <section className={styles.profileSection}>
-          <img src="/img/Profile.png" className={styles.avatar} />
-          <div className={styles.profileInfo}>
-            <h2><span className={styles.name}>0l_ha3</span><button>프로필 편집</button></h2>
-            <span className={styles.profileStat}>게시물 3 </span>
-            <span className={styles.profileStat}>팔로워 0 </span>
-            <span className={styles.profileStat}>팔로우 0 </span>
+        <section className={styles.profileInfo}>
+          <img
+            src="/img/Profile.png"
+            alt="profile"
+            className={styles.avatar}
+          />
+          <div className={styles.profileText}>
+            <div className={styles.userId}>0l_ha3</div>
+            <button className={styles.editBtn}>프로필 편집</button>
+            <div className={styles.stats}>
+              <span>게시물 0</span>
+              <span>팔로워 0</span>
+              <span>팔로우 0</span>
+            </div>
           </div>
         </section>
 
-        <hr className={styles.divider} />
-
-        <section className={styles.gridSection}>
-          <div className={styles.grid}>
-            <div className={styles.post}>
-              <img src="/img/1.JPG" alt="Post 1" />
+        <section className={styles.grid}>
+          {posts.map((post) => (
+            <div key={post.id} className={styles.postBox}>
+              {post.image ? (
+                <img src={post.image} alt="post" className={styles.postImg} />
+              ) : (
+                <div className={styles.placeholder}></div>
+              )}
             </div>
-            <div className={styles.post}>
-              <img src="/img/2.JPG" alt="Post 2" />
-            </div>
-            <div className={styles.post}>
-              <img src="/img/3.jpeg" alt="Post 3" />
-            </div>
-            <div className={styles.post}></div>
-            <div className={styles.post}></div>
-            <div className={styles.post}></div>
-          </div>
+          ))}
         </section>
       </main>
     </div>
